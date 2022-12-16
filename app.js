868c130f96755fdf27aad448867bb13f6b94492c
Sandeep Singh-GUVA <Sandeep_Singh-GUVA@external.mckinsey.com>
2022-12-16 12:35:36 +0530

    node course - create first server

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 //console.log('sandeep singh');
 
-for (let index = 0; index<10; index++)
-{
-    console.log(index);
-}
+// for (let index = 0; index<10; index++)
+// {
+//     console.log(index);
+// }
+
+
+// const http = require('node:http');
+
+// // Create a local server to receive data from
+// const server = http.createServer((req, res) => {
+//     res.write("hello world");
+//   res.end();
+// });
+
+// server.listen(8000);
+
+
+const http = require('node:http');
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!',
+  }));
+});
+
+server.listen(8000);
